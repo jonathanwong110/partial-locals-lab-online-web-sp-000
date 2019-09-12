91ad5120b2c0768c962b6dc83da9ccdbde9f5d73
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-12 14:32:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -26,6 +26,6 @@ class StudentsController < ApplicationController
   end
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown, :query)
+    params.require(:student).permit(:name, :birthday, :hometown)
   end
 end
